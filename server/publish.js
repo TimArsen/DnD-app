a8a67c2a05c82ac7a0e4dc4c231a74e0e1d01e4a
Tim Arsenault <timarsen@gmail.com>
2016-06-22 11:49:02 -0600

    add monster publishing

--- server/publish.js
+++ server/publish.js
@@ -10,3 +10,12 @@ Meteor.publish('characterDetails', function(id){
 Meteor.publish('combat', function(){
 	return Characters.find({inCombat: true});
 });
+
+Meteor.publish('monsters', function(){
+	return Monsters.find({});
+});
+
+Meteor.publish('monsterDetails', function(id){
+	check(id, String);
+	return Monsters.find({_id: id});
+});
